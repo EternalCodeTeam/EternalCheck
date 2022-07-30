86949f6e1fb662f97e2b756bc02744eacd20e510
Osnixer <Osnixeroo@gmail.com>
2022-07-30 16:21:27 +0200

    fixed import

--- src/main/java/com/eternalcode/check/EternalCheck.java
+++ src/main/java/com/eternalcode/check/EternalCheck.java
@@ -2,7 +2,6 @@ package com.eternalcode.check;
 
 import com.eternalcode.check.command.argument.PlayerArgument;
 import com.eternalcode.check.command.argument.UserArgument;
-import com.eternalcode.check.command.contextual.PlayerContextual;
 import com.eternalcode.check.command.implementation.AdmitCommand;
 import com.eternalcode.check.command.implementation.CheckCommand;
 import com.eternalcode.check.command.message.InvalidUseMessage;
@@ -10,17 +9,16 @@ import com.eternalcode.check.command.message.PermissionMessage;
 import com.eternalcode.check.config.ConfigManager;
 import com.eternalcode.check.config.implementation.MessagesConfig;
 import com.eternalcode.check.config.implementation.PluginConfig;
-import com.eternalcode.check.listener.AsyncPlayerChatListener;
-import com.eternalcode.check.listener.PlayerCommandPreprocessListener;
-import com.eternalcode.check.listener.PlayerMoveListener;
-import com.eternalcode.check.listener.PlayerQuitListener;
+import com.eternalcode.check.controller.CheckedPlayerChatController;
+import com.eternalcode.check.controller.CheckedPlayerCommandController;
+import com.eternalcode.check.controller.CheckedPlayerMoveController;
+import com.eternalcode.check.controller.LogoutPunishmentController;
 import com.eternalcode.check.shared.legacy.LegacyColorProcessor;
 import com.eternalcode.check.user.User;
 import com.eternalcode.check.user.UserService;
-import com.eternalcode.check.user.UserTask;
 import dev.rollczi.litecommands.LiteCommands;
 import dev.rollczi.litecommands.bukkit.LiteBukkitFactory;
-import dev.rollczi.litecommands.scheme.SchemeFormat;
+import dev.rollczi.litecommands.bukkit.tools.BukkitOnlyPlayerContextual;
 import net.kyori.adventure.platform.AudienceProvider;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.minimessage.MiniMessage;
@@ -34,7 +32,7 @@ import java.util.stream.Stream;
 
 public final class EternalCheck extends JavaPlugin {
 
-    public static EternalCheck instance;
+    private static EternalCheck instance;
 
     private ConfigManager configManager;
     private PluginConfig config;
@@ -75,14 +73,13 @@ public final class EternalCheck extends JavaPlugin {
         this.userService = new UserService();
 
         this.liteCommands = LiteBukkitFactory.builder(this.getServer(), "EternalCheck")
-                .argument(Player.class, "player", new PlayerArgument(this.messages, server))
-                .argument(User.class, "user", new UserArgument(this.messages, this.userService, server))
+                .argument(Player.class, new PlayerArgument(this.messages, server))
+                .argument(User.class, new UserArgument(this.messages, this.userService, server))
 
-                .contextualBind(Player.class, new PlayerContextual())
+                .contextualBind(Player.class, new BukkitOnlyPlayerContextual(""))
 
                 .permissionHandler(new PermissionMessage(this.messages, this.notificationAnnouncer))
                 .invalidUsageHandler(new InvalidUseMessage(this.messages, this.notificationAnnouncer))
-                .schemeFormat(SchemeFormat.ARGUMENT_ANGLED_OPTIONAL_SQUARE)
 
                 .commandInstance(new AdmitCommand(this.messages, this.config, this.userService, server, this.notificationAnnouncer))
                 .commandInstance(new CheckCommand(this.configManager, this.messages, this.config, this.userService, server, this.notificationAnnouncer))
@@ -90,18 +87,18 @@ public final class EternalCheck extends JavaPlugin {
                 .register();
 
         Stream.of(
-                new AsyncPlayerChatListener(this.config, this.userService),
-                new PlayerCommandPreprocessListener(this.messages, this.config, this.userService, this.notificationAnnouncer),
-                new PlayerMoveListener(this.config, this.userService),
-                new PlayerQuitListener(this.messages, this.config, this.userService, server, this.notificationAnnouncer)
-        ).forEach(listener -> this.getServer().getPluginManager().registerEvents(listener, this));
+                new CheckedPlayerChatController(this.config, this.userService),
+                new CheckedPlayerCommandController(this.messages, this.config, this.userService, this.notificationAnnouncer),
+                new CheckedPlayerMoveController(this.config, this.userService),
+                new LogoutPunishmentController(this.messages, this.config, this.userService, server, this.notificationAnnouncer)
+        ).forEach(listener -> server.getPluginManager().registerEvents(listener, this));
 
         if (!this.config.settings.runnable.enabled) {
             return;
         }
 
         server.getScheduler().runTaskTimerAsynchronously(this,
-                new UserTask(this.messages, this.config, this.userService, this.notificationAnnouncer),
+                new CheckNotificationTask(this.messages, this.config, this.userService, this.notificationAnnouncer),
                 0L, 20L * this.config.settings.runnable.interval
         );
 
